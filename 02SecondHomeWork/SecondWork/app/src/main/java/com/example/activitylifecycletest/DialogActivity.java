6dbde1047883973120f919dc8c06159c3a71bf84
DESKTOP-QCG4H1F\wlm <1776809811@qq.com>
2020-10-06 10:44:51 +0800

    之前运行不出来结果，发现问题出现在DialogActivity.java和NormalActivity.java文件中少配置文件，加入之后就完整了

--- 02SecondHomeWork/SecondWork/app/src/main/java/com/example/activitylifecycletest/DialogActivity.java
+++ 02SecondHomeWork/SecondWork/app/src/main/java/com/example/activitylifecycletest/DialogActivity.java
@@ -9,5 +9,6 @@ public class DialogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
     }
 }
--- 02SecondHomeWork/SecondWork/app/src/main/java/com/example/activitylifecycletest/NormalActivity.java
+++ 02SecondHomeWork/SecondWork/app/src/main/java/com/example/activitylifecycletest/NormalActivity.java
@@ -9,5 +9,6 @@ public class NormalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.normal_layout);
     }
 }
