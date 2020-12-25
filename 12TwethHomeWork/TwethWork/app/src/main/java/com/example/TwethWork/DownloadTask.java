4d26389b6cdc60a19f3ad55cc840977f0ce5b404
DESKTOP-QCG4H1F\wlm <1776809811@qq.com>
2020-12-25 09:17:57 +0800

    查找12次作业出现的错误

--- 12TwethHomeWork/TwethWork/app/src/main/java/com/example/TwethWork/DownloadTask.java
+++ 12TwethHomeWork/TwethWork/app/src/main/java/com/example/TwethWork/DownloadTask.java
@@ -14,7 +14,6 @@ public class DownloadTask extends AsyncTask<String, Integer, Integer> {
     public static final int TYPE_FAILED = 1;
     public static final int TYPE_PAUSED = 2;
     public static final int TYPE_CANCELED = 3;
-
     private DownloadListener listener;
     private boolean isCanceled = false;
     private boolean isPaused = false;
